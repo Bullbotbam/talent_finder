426067ab62fee48b8464c428b6c1156165694735
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-07-29 00:48:41 -0500

    edit schema

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
 	user: 'root',
 	// Your MySQL password
 	password: 'Fr334m3!',
-	database: 'talent-finder',
+	database: 'talent',
 });
 
 module.exports = db;
--- db/schema.sql
+++ db/schema.sql
@@ -1,34 +1,36 @@
-DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS employee_roles;
+DROP TABLE IF EXISTS departments;
 
-CREATE TABLE employee (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER,
-  manager_id INTEGER,
-  CONSTRAINT fk_voter FOREIGN KEY (role_id) REFERENCES
-  role(id) ON DELETE CASCADE,
-  CONSTRAINT fk_voter FOREIGN KEY (manager_id) REFERENCES
-  managers(id) ON DELETE CASCADE,
+CREATE DATABASE talent;
+
+USE talent;
+
+CREATE TABLE departments(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  department_name VARCHAR(30)
 );
 
-CREATE TABLE role (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-department_id INTEGER NOT NULL,
-  CONSTRAINT fk_party
+CREATE TABLE employee_roles(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL(10,2),
+department_id INTEGER,
+  CONSTRAINT fk_employees
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES departments(id)
     ON DELETE SET NULL
 );
 
-CREATE TABLE department (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-  
+CREATE TABLE employees(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INTEGER,
+  manager_id INTEGER,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES
+  employee_roles(id) ON DELETE CASCADE,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES
+  employees(id) ON DELETE CASCADE
 );
-
 
